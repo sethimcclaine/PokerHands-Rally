93351ec769b19fc39e6454b1e97ba37401447392
Seth McClaine <sethmcclaine@Seths-MacBook-Pro.local>
2015-11-10 14:01:16 -0700

    update dealer to prototype player 	deleted:    models/.Hand.js.swp 	modified:   models/Dealer.js

--- models/Dealer.js
+++ models/Dealer.js
@@ -1,42 +1,19 @@
-
-
 var Hand = require("./Hand.js");
 var Deck = require("./Deck.js");
+var Player = require("./Player"); 
 
-module.exports = function (dealerInfo) {
-    /*
-    this = new Player(dealerInfo);
-    /*/
-    //Begin Player Data
-    var name = dealerInfo.name;
-    var hand = new Hand();
+var Dealer = Player;
 
-    this.addToHand = function(card) {
-        //hand.cards.push(card);
-        hand.addCard(card);
-    };
-    this.showHand = function() {
-        hand.show();
-    };
-    this.evaluateHand = function() {
-        hand.evaluate();
-    };
-    this.sayMyName = function() {
-        console.log(name + ' (Dealer)');
-    };
-    //End player data
-    //*/
-    this.getDeck = function() {
-        this.currentDeck = new Deck();
-    };
-    this.shuffleDeck = function() {
-        this.currentDeck.shuffle();
-    };
-
-    this.dealCardToPlayer = function(player) {
-        var topCard = this.currentDeck.drawCard();
-        player.addToHand(topCard);
-    };
-
-    return this;
+Dealer.prototype.getDeck = function() {
+    this.currentDeck = new Deck();
 };
+Dealer.prototype.shuffleDeck = function() {
+    this.currentDeck.shuffle();
+};
+
+Dealer.prototype.dealCardToPlayer = function(player) {
+    var topCard = this.currentDeck.drawCard();
+    player.addToHand(topCard);
+};
+
+module.exports = Dealer;
